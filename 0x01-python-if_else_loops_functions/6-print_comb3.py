ec0bcef74adbae6a6b3562647d86b75d19aed967
Essy <esthercikechukwu@gmail.com>
2023-11-01 01:13:35 +0000

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-for i in range(9):
+for i in range(0, 10):
 
    for j in range(i + 1, 10):
     
     if i * 10 + j < 89:
+        print("{}{}".format(i, j))
+    else:
 
-     print("{:d}{:d}".format(i, j), end=", ") 
+     print("{}{}".format(i, j), end=", ") 
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,9 +4,9 @@ def uppercase(str):
 
     for i in str:
 
-        if ord("a") <= ord(i) <= ord("z"):
+        if ord('a') <= ord(i) <= ord('z'):
 
-            i = chr(ord(i) - 32)
+             i = chr(ord(i) - 32)
+print("{:s}".format(i), end="")
 
-        print("{:s}".format(i), end="")
-        print()
+print ()
