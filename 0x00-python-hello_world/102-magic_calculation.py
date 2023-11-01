fbf6208fdeed179ac8c55bf4392c7eb9e75f2991
Essy <esthercikechukwu@gmail.com>
2023-11-01 19:17:20 +0000

    magic_calc

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-import dis
 def magic_calculation(a, b):
-result = 98
-result = result ** a
-result = result + b
-return result
+
+    result = 98
+    result = result ** a
+    result = result + b
+    return result
+
+a = 2
+b = 3
+expected_result = 98 ** a + b
+result = magic_calculation(a, b)
+print(result)
