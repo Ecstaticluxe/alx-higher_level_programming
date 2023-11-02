50a77f916b2131b88f678e89780fe0699950a2ec
Essy <esthercikechukwu@gmail.com>
2023-11-02 04:12:08 +0000

    102-magic

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    if a < b:
-        return 0
-    elif b >= a:
-        return 1
-        return -1
+
+        result = 98 + a ** b
+            return result
+
