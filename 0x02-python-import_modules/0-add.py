5ce849d0f28a4de6aeb661fc8892ce47b05a05db
Essy <esthercikechukwu@gmail.com>
2023-11-02 21:46:56 +0000

    var

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def add(a, b):
-    import add_0.py
-a = 1
-b = 2
-c = add_0.py(1 + 2)
-print(c)
+
+if _name_ == '_main_':
+    from add_0 import add
+    a = 1
+    b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
