7677bf6dbde939fe736e4d574448264583e86a30
Essy <esthercikechukwu@gmail.com>
2023-11-04 11:59:20 +0000

    varload

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-if _name_ == "_main_":
-    from variable_load_5 import a
-    print(a)
+
+import importlib
+module = importlib.import_module('variable_load_5', package=None)
+a_value = getattr(module, 'a')
+print(a_value)
