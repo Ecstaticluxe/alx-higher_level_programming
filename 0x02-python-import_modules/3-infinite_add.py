ce500fc9c12b8f23bdc52f10fa0baf78fb2976ec
Essy <esthercikechukwu@gmail.com>
2023-11-04 12:31:34 +0000

    3inf

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 import sys
-
 def add_args(argv):
-    total = sum(int(arg) for arg in argv[1:])
-    print(total)
-
 if __name__ == "__main__":
     add_args(sys.argv)
+    total = sum(int(arg) for arg in argv[1:])
+        print(total)
