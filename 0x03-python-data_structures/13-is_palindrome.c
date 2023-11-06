23668cde3067d2cd864afdf98681795c536c087a
Essy <esthercikechukwu@gmail.com>
2023-11-06 16:14:29 +0000

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,50 +3,53 @@
 #include <stdio.h>
 
 /**
- * is_palindrome -  checks if a singly linked list is a palindrome
- * @head: head of list
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- */
+*add_nodeint - adds a new node at the beginning of a listint_t list
+*@head: head of listint_t
+*@n: int to add in listint_t list
+*Return: address of the new element, or NULL if it failed
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
+/**
+* is_palindrome -  checks if a singly linked list is a palindrome
+* @head: head of list
+* Return: 0 if it is not a palindrome, 1 if it is a palindrome
+*/
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *fast = *head;
-	listint_t *slow = *head;
+	listint_t *head2 = *head;
+	listint_t *aux = NULL, *aux2 = NULL;
 
-	if (*head == NULL || (*head)->next == NULL)
+	if (*head == NULL || head2->next == NULL)
+		return (1);
+	while (head2 != NULL)
 	{
-	return (1);
+		add_nodeint(&aux, head2->n);
+		head2 = head2->next;
 	}
-	while (fast != NULL && fast->next != NULL)
-{
-	fast = fast->next->next;
-	slow = slow->next;
-}
-	if (fast != NULL)
-
-	slow = slow->next;
-
-listint_t *current = slow;
-listint_t *next = NULL;
-listint_t *new_head = NULL;
-
-	while (current != NULL)
+	aux2 = aux;
+	while (*head != NULL)
 	{
-	next = current->next;
-	current->next = new_head;
-	new_head = current;
-	current = next;
+		if ((*head)->n != aux2->n)
+		{
+			free_listint(aux);
+			return (0);
+		}
+		*head = (*head)->next;
+		aux2 = aux2->next;
 	}
-
-	listint_t *left = *head;
-	listint_t *right = new_head;
-
-	while (left != NULL && right != NULL)
-	{
-	if (left->n != right->n)
-	return (0);
-}
-left = left->next;
-	right = right->next;
+	free_listint(aux);
 	return (1);
 }
