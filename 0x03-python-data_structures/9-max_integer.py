9f2c0d5f885af01882f8f5c6d1b343ca7eb9e860
Essy <esthercikechukwu@gmail.com>
2023-11-06 14:57:02 +0000

    10-divisible_by_2.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-max_integer = __import__('9-max_integer').max_integer
 
-my_list = [1, 90, 2, 13, 34, 5, -13, 3]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    
+    max_value = my_list[0]
+    for value in my_list:
+        if value > max_value:
+            max_value = value
+    
+    return max_value
