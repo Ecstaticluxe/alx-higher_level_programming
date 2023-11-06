4080a624b90a047821cf885f3d911bf8cb754934
Essy <esthercikechukwu@gmail.com>
2023-11-06 14:35:17 +0000

    8-mul

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    sen_len = len(sentence)
-    if sen_len == 0:
+    if not sentence:
         sentence = None
+    if sentence:
+        sen_len = len(sentence)
     else:
-        return sen_len, sentence if not sentence else sentence[:1]
+        sen_len = 0
+    return (sen_len, sentence if not sentence else sentence[:1])
