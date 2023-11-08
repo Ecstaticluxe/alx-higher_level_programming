ad9499d33207f1d2390b909d33cce23d9e6df983
Essy <esthercikechukwu@gmail.com>
2023-11-08 17:19:43 +0000

    2args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    import sys
 
-    import sys
+    argLen = len(sys.argv)
 
-    argLen = len(sys.argv)
+    if argLen == 1:
+        print("{} arguments.".format(argLen - 1))
+    elif argLen == 2:
+        print("{} argument:".format(argLen - 1))
+    else:
+        print("{} arguments:".format(argLen - 1))
 
-    if argLen == 1:
+    for i in range(1, argLen):
+        print("{}: {}".format(i, sys.argv[i]))
 
-        print("{} arguments.".format(argLen - 1))
-
-    elif argLen == 2:
-
-        print("{} argument:".format(argLen - 1))
-
-    else:
-
-        print("{} arguments:".format(argLen - 1))
-
-    for i in range(1, argLen):
-
-        print("{}: {}".format(i, sys.argv[i]))
