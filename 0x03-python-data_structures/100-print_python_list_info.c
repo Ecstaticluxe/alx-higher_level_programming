d2e64971b2c4e17d3e8986c593cec0b43913f43d
Essy <esthercikechukwu@gmail.com>
2023-11-08 03:45:04 +0000

    python3 test

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,42 +1,22 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <stdio.h>
+
+/**
+ * print_python_list_info -  function that prints some basic
+ * info about Python lists
+ * @p: python list
+ */
 
 void print_python_list_info(PyObject *p)
 {
-    Py_ssize_t size, alloc, i;
-    PyObject *item;
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *obj = (PyListObject *)p;
 
-    size = PyList_Size(p);
-    alloc = ((PyListObject *)p)->allocated;
-
-    printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", alloc);
-
-    for (i = 0; i < size; i++)
-    {
-        item = PyList_GetItem(p, i);
-        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-    }
-}
-
-int main(int argc, char *argv[])
-{
-    Py_Initialize();
-    if (argc == 2)
-    {
-        PyObject *pName, *pModule, *pDict, *pFunc, *pArgs, *pValue;
-        if (PyList_Check(pValue))
-        {
-            pValue = Py_BuildValue("i", atoi(argv[1]));
-            print_python_list_info(pValue);
-        }
-        else
-            fprintf(stderr, "First argument is not a list\n");
-    }
-    else
-        fprintf(stderr, "Usage: %s number\n", argv[0]);
-
-    Py_Finalize();
-    return (0);
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < size; i++)
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
 }
--- 0x03-python-data_structures/Python.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef Py_OBJECT_H
-#define Py_OBJECT_H
-#ifdef __cplusplus
-void print_python_list_info(PyObject *p);
-
-int main(int argc, char *argv[])
-extern "C" {
-#endif
