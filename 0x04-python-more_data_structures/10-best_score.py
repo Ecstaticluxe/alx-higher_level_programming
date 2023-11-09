0031d41542482498b42751595fc636c8b1a077ba
Essy <esthercikechukwu@gmail.com>
2023-11-09 04:29:57 +0000

    10main

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    max_key = None
-    max_value = float('-inf')
 
-    for key, value in a_dictionary.items():
-        if value > max_value:
-            max_key = key
-            max_value = value
-
-    return max_key
+    max_score_key = max(a_dictionary, key=a_dictionary.get)
+    return max_score_key
