95763dc6f4467bcf2628115e86df0bf89a7c5fd3
Essy <esthercikechukwu@gmail.com>
2023-11-20 14:26:21 +0000

    6

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def raise_exception():
+
+    try:
+        result = "string" + 5
+        except TypeError as e:
+        print("TypeError:", e)
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+
+   def raise_exception_msg(message=""):
+    try:
+
+        undefined_variable
+    except NameError as e:
+
+        print("Name error:", message)
+
+        raise NameError(message) from e 
