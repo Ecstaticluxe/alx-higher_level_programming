d0ef9095d82bb1c1bc318ab8f083acfb4a65531f
Essy <esthercikechukwu@gmail.com>
2023-11-21 04:02:48 +0000

    103

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,52 @@
+#include <Python.h>
+
+void print_python_list(PyObject *p) {
+    Py_ssize_t size, i;
+
+    if (!PyList_Check(p)) {
+        fprintf(stderr, "Invalid List Object\n");
+        return;
+    }
+
+    size = PyList_Size(p);
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; ++i) {
+        printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+    }
+}
+
+void print_python_bytes(PyObject *p) {
+    Py_ssize_t size, i;
+    char *bytes;
+
+    if (!PyBytes_Check(p)) {
+        fprintf(stderr, "Invalid Bytes Object\n");
+        return;
+    }
+
+    size = PyBytes_Size(p);
+    bytes = PyBytes_AsString(p);
+
+    printf("[.] bytes object info\n");
+    printf("  size: %ld\n", size);
+    printf("  trying string: %s\n", bytes ? bytes : "(error)");
+
+    printf("  first %ld bytes:", size > 10 ? 10 : size);
+    for (i = 0; i < size && i < 10; ++i) {
+        printf(" %02hhx", bytes[i]);
+    }
+    printf("\n");
+}
+
+void print_python_float(PyObject *p) {
+    printf("[.] float object info\n");
+    if (!PyFloat_Check(p)) {
+        fprintf(stderr, "Invalid Float Object\n");
+        return;
+    }
+
+    printf("  value: %f\n", ((PyFloatObject *)p)->ob_fval);
+}
