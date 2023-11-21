0db1b6e46dccadf73913098f1e29eeb1a7cfa373
Essy <esthercikechukwu@gmail.com>
2023-11-21 19:15:37 +0000

    task1-6

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3:x
-:x
-
+"""Define a class Square."""
 
 class Square:
+    """Represent a square."""
+
     def __init__(self, size=0):
         """Initialize a Square instance.
 
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
+"""Define a class square."""
 
 class Square:
+    """Represent a square."""
+
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a Square instance.
 
