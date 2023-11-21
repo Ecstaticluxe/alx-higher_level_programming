625704403d0d67cd4509d3b0e2b2d3ad7a2f97b2
Essy <esthercikechukwu@gmail.com>
2023-11-21 19:28:34 +0000

    task1-6

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-python3 -c 'print(__import__("my_module").MyClass.__doc__)'
-
 """Define a class Square."""
 class Square:
     """Represent a square."""
     pass
+
+python3 -c 'print(__import__("my_module").MyClass.__doc__)'
