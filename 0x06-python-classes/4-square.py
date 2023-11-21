cc753007adec5b79d2905ae114e543d60922857a
Essy <esthercikechukwu@gmail.com>
2023-11-21 20:09:54 +0000

    all

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3:x
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
 
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -2,6 +2,7 @@
 
 """DEfine a class Square."""
 
+
 class Square:
     """Represent a square."""
 
