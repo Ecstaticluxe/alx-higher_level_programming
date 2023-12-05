f487a050d0d8cbdb9f6f831c17781b5a26207e1e
Essy <esthercikechukwu@gmail.com>
2023-12-05 19:11:34 +0000

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,13 +3,11 @@
 
 
 def read_file(filename=""):
-    '''a function that reads a text file (UTF8) and prints it to stdout.'''
-
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            content = file.read()
-            print(content)
-            '''raises exception'''
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    '''a function that reads a text file (UTF8) and prints it to stdout.
+    Args:
+    filename (str): name of the file
+    '''
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.read()
+        print(content)
 
