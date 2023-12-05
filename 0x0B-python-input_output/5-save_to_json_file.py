71e99e3a34180d1126e9858090365c8eaf85d28d
Essy <esthercikechukwu@gmail.com>
2023-12-05 23:43:12 +0000

    task 2-5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 """An object text file"""
-
-
 import json
+
 
 def save_to_json_file(my_obj, filename):
     """Writes an Object to a text file, using a JSON representation."""
