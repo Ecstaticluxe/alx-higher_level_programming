1bc03d9408cfa92922c187b569c3546df311eba4
Essy <esthercikechukwu@gmail.com>
2023-12-05 22:55:32 +0000

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,11 +3,7 @@
 
 
 def read_file(filename=""):
-    '''a function that reads a text file (UTF8) and prints it to stdout.
-    Args:
-    filename (str): name of the file
-    '''
-    with open(filename, 'r', encoding='utf-8') as file:
-        content = file.read()
-        print(content)
+    '''a function that reads a text file (UTF8) and prints it to stdout.'''
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end="")
 
