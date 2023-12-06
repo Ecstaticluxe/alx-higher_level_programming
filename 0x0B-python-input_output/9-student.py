2720abd6857f6098dcc6b68655359be27db43070
Essy <esthercikechukwu@gmail.com>
2023-12-06 00:51:39 +0000

    9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,15 +4,15 @@
 
 
 class Student:
-    """A class that defines a student"""
+    """Represents a student"""
 
 
-def __init__(self, first_name, last_name, age):
-    """Initialise a new Student.
+    def __init__(self, first_name, last_name, age):
+    """Initialize a new Student.
     Args:
-    first_name (str): the first name.
-    last name (str): the last name.
-    age (int): the age of the student.
+        first_name (str): the first name.
+        last name (str): the last name.
+        age (int): the age of the student.
     """
     self.first_name = first_name
     self.last_name = last_name
@@ -20,5 +20,4 @@ def __init__(self, first_name, last_name, age):
 
     def to_json(self):
         """retrieves a dictionary representation of a Student instance"""
-        __import__("8-class_to_json_file")
         return self
