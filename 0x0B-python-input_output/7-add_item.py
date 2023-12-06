1e0b44d5385c36aa3db547c25d696ab87698db65
Essy <esthercikechukwu@gmail.com>
2023-12-06 00:45:32 +0000

    mandatory

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,8 +3,9 @@
 """ 
 adds all arguments to a Python list, and then save them to a file.
 """
-import sys
 
+
+import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
