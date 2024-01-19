0a1909f06394f1f36067b0e6b0242e90221ef842
Esther <esthercikechukwu@gmail.com>
2024-01-18 17:06:16 -0800

    tasks 0-9

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- create the table force_name
-CREATE_TABLE_QUERY="CREATE TABLE IF NOT EXISTS $DB_NAME.force_name (
+CREATE TABLE IF NOT EXISTS $DB_NAME.force_name (
 	id INT,
 	name VARCHAR(256) NOT NULL
 );
