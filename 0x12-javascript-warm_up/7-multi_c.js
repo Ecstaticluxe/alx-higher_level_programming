11b641e3b0e006ec79992ae01754bc787283acd4
Essy <esthercikechukwu@gmail.com>
2024-02-12 18:40:13 +0000

    File 3

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const [,, x] = process.argv;
+const num = parseInt(x);
+
+if (!isNaN(num)) {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+		    }
+} else {
+  console.log('Missing number of occurrences');
+}
