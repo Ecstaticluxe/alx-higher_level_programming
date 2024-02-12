4ca3a23084e2cb0ccd0ff8396a89a052b13fa94e
Essy <esthercikechukwu@gmail.com>
2024-02-12 19:38:39 +0000

    File 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 
-function factorial (n) {
-	if (isNaN(parseInt(n))) {
-		return (1);
+function factorial(n) {
+	if (isNaN(n) || n < 0) {
+		return 1;
 	}
-	n = parseInt(n);
 
-	if (n === 0 || n === 1) {
-		return (1);
-	} else {
-		return (n * factorial(n - 1));
-	}
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+
+        return n * factorial(n - 1);
+    }
 }
 
-const input = process.argv[2];
+
+const input = parseInt(process.argv[2]);
 console.log(factorial(input));
