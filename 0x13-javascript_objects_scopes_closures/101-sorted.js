876a8e3a46401ce86e008e29e22350a418ed2eb9
Essy <esthercikechukwu@gmail.com>
2024-02-14 04:42:58 +0000

    update

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
-const { dict } = require('./101-data');
-const usersByOccurrence = {};
-for (const userId in dict) {
-	    const occurrence = dict[userId];
-	    if (!usersByOccurrence[occurrence]) {
-		            usersByOccurrence[occurrence] = [];
-		        }usersByOccurrence[occurrence].push(userId);
-}console.log(usersByOccurrence);
+const dict = require('./101-data.js').dict;
+
+const newDict = {};
+
+Object.getOwnPropertyNames(dict).forEach(occurences => {
+  if (newDict[dict[occurences]] === undefined) {
+    newDict[dict[occurences]] = [occurences];
+  } else {
+    newDict[dict[occurences]].push(occurences);
+  }
+});
+console.log(newDict);
