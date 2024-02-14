1487c88c3ad89230f442e92652b55c0135f8da0a
Essy <esthercikechukwu@gmail.com>
2024-02-14 05:10:57 +0000

    update

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
 
 function factorial(n) {
-	if (isNaN(n) || n < 0) {
-		return 1;
-	}
+	return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 
-    if (n === 0 || n === 1) {
-        return 1;
-    } else {
-
-        return n * factorial(n - 1);
-    }
 }
 
 
